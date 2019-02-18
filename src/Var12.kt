d825e00da13b08f12dc71a64574f12073e94de75
Игорь <exlight228@gmail.com>
2019-02-18 16:20:10 +0300

    Initial commit

---
+++ src/Var12.kt
@@ -0,0 +1,6 @@
+fun main () {
+    val chelik = Var12N1("Игорь", "Козлов", 19)
+    println(chelik.firstName)
+    println(chelik.lastName)
+    println(chelik.age)
+}
---
+++ src/Var12N1.kt
@@ -0,0 +1,1 @@
+class Var12N1 (var firstName: String, var lastName: String, var age: Int)
