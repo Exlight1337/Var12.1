b4d5cb2664c9035c01bd9e87c3e5400789fea404
Игорь <exlight228@gmail.com>
2019-02-24 15:52:27 +0300

    Мучаюсь с добавлением в массив. Задатки есть, ищу решение

--- src/Var12N1.kt
+++ src/Var12N1.kt
@@ -1,7 +1,7 @@
 
 class roooms(val name: String, val street: String, val number: String, val cvar: String) {
     override fun toString() = "($name, $street, $number, $cvar )"
-    var c = Dobav("Качоткин", "Пушкинская", "150", "50")
+    var s = Dobav("Качоткин", "Пушкинская", "150", "50")
 
 }
     
