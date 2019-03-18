8582662499d8908786ea7207cba636db2eb31e72
Игорь <exlight228@gmail.com>
2019-03-18 19:16:52 +0300

    Переделал функции метода удаления и замены адреса. Исправил тесты, добавил пакет для класса. Переименовал название функций и переменных.

--- src/ClassofBook/bookofaddress.kt
+++ src/ClassofBook/bookofaddress.kt
@@ -1,11 +1,11 @@
 @file:Suppress("UNUSED_PARAMETER", "unused")
 package ClassofBook
+
 class bookofaddress {
 
     data class Adress (var adress: String, var house: String, var appartament: String)
 
     data class bookOfName (var adList: MutableMap<String, Adress>) {
-
 
         fun addresOfNames(name: String): Adress {
            val adName = adList[name] ?: throw Exception()
@@ -17,19 +17,17 @@ class bookofaddress {
                 adList[person.first] = person.second
         }
 
-        fun delete(name1: String):Boolean {
-            return if (adList.containsKey(name1)){
+        fun delete(name1: String):Boolean =
+            if (adList.containsKey(name1)){
                 adList.remove(name1)
                 true
             } else false
-        }
 
-        fun changeAdressOfPeople(person: Pair<String, Adress>):Boolean {
-          return if (adList.containsKey(person.first)){
+        fun changeAdressOfPeople(person: Pair<String, Adress>):Boolean =
+            if (adList.containsKey(person.first)){
                 adList[person.first] = person.second
               true
             }  else false
-        }
 
         fun inTheStreet(adress: String): List<String> {
             val AdressList = mutableListOf<String>()
@@ -46,5 +44,5 @@ class bookofaddress {
                     HouseList.add(Str.key)
             return HouseList
         }
+        }
     }
-}
