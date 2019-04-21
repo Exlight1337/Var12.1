537effcc12d7706fc4b9f70dbb6bb2d984316d7c
Игорь <exlight228@gmail.com>
2019-04-21 13:14:21 +0300

    Начало+продолжение

--- src/Novella/ResOfNovel.kt
+++ src/Novella/ResOfNovel.kt
@@ -4,12 +4,11 @@ import jcurses.system.Toolkit
 import java.io.File
 
 
-class ResOfNovel () {
+class ResOfNovel {
     var c = ""
     fun readBeginning () {
         val begin = File("C://Users/Igor/IdeaProjects/Var12/Begin.txt").readText()
         println(begin)
-
 
             println("1)println()")
             println("2)prentln()")
@@ -22,6 +21,21 @@ class ResOfNovel () {
                 "2" -> println(File("C://Users/Igor/IdeaProjects/Var12/12.txt").readText())
                 "3" -> println(File("C://Users/Igor/IdeaProjects/Var12/13.txt").readText())
             }
-
         }
+    fun continued () {
+        var a = ""
+        val continuation = File("C://Users/Igor/IdeaProjects/Var12/Continued.txt").readText()
+        println(continuation)
+
+        println("1)К типу Int")
+        println("2)К типу Double")
+        println("3)К типу Char")
+
+        a = readLine().toString()
+        when(a) {
+            "1" -> println(File("C://Users/Igor/IdeaProjects/Var12/21.txt").readText())
+            "2" -> println(File("C://Users/Igor/IdeaProjects/Var12/22.txt").readText())
+            "3" -> println(File("C://Users/Igor/IdeaProjects/Var12/23.txt").readText())
+        }
+    }
     }
--- src/Novella/main.kt
+++ src/Novella/main.kt
@@ -3,4 +3,6 @@ package Novella
 fun main (args: Array<String>){
      val begin = ResOfNovel()
        begin.readBeginning()
+     val cont = ResOfNovel()
+       cont.continued()
     }
