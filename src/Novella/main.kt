0a8569846b1e89c29e321f252631ae1044c9dffc
Игорь <exlight228@gmail.com>
2019-09-07 16:51:20 +0300

    коммит

--- src/Novella/main.kt
+++ src/Novella/main.kt
@@ -4,22 +4,8 @@ import com.xenomachina.argparser.ArgParser
 import java.io.PrintStream
 import java.io.ByteArrayOutputStream
 
-
-
-
-
-
-
 fun main1 (args: Array<String>){
-    ArgParser(args).parseInto(::ResOfNovel).writting()
-    val baos = ByteArrayOutputStream()
-    val ps = PrintStream(baos)
-    val old = System.out
-    System.setOut(ps)
-    println("Правильных ответов =2")
-    System.out.flush()
-    System.setOut(old)
-    println(baos.toString())
+   val a = ResOfNovel().readNovel()
 }
 
 
